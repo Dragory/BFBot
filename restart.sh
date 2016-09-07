87c8fe1d83d52cd6121bddd0c3310bba9709261e
Miikka Virtanen <dragory.up@gmail.com>
2016-09-07 08:35:21 +0300

    meta: load .bashrc for nvm in start/restart scripts

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. ~/.bashrc # Load NVM
 nvm use
 forever stop "discord-bfbot" # Stop existing bot instances
 forever -a -l log.log -o out.log -e err.log -u "discord-bfbot" start src/index.js # Start a new bot instance
